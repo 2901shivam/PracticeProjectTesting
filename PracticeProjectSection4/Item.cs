7b3d2eb745acef2a559b32bd138cf327b9c4ad78
2901shivam <shivamsingh201617@gmail.com>
2024-01-18 10:48:05 +0530

    AutomaticTesting

---
+++ PracticeProjectSection4/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProjectSection4
+{
+    public  class Item
+    {
+        public int Id { get; set; }
+
+        public string Iname { get; set; }
+
+        public int IPrice { get; set; }
+
+
+    }
+}
---
+++ PracticeProjectSection4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeProjectSection4;
+
+Console.WriteLine("List Of Item");
+
+List<Item> items = new List<Item>()
+{
+    new Item(){Id=1,Iname="Panner Tikka",IPrice=150},
+    new Item(){Id=2,Iname="Panner Masala",IPrice=250},
+    new Item(){Id=3,Iname="Button Mashroom",IPrice=350},
+    new Item(){Id=4,Iname="Chole khulche",IPrice=50},
+    new Item(){Id=5,Iname="Mix Veg",IPrice=150},
+};
+
+foreach(Item it in items)
+{
+    Console.WriteLine("ID:"+it.Id);
+    Console.WriteLine("Item Name:" + it.Iname);
+    Console.WriteLine("Item Price:" + it.IPrice);
+
+}
